9feed82499901f203d9fda6295658f2cfb008fbb
VIKTOR VENEDIKTOV <shinijin@gmail.com>
2024-12-26 12:27:49 +0400

    0.0.0.0 host added

--- python/script.py
+++ python/script.py
@@ -72,7 +72,7 @@ def main():
 
     # Запускаем функцию ожидания
     wait(delay_seconds)
-    app.run(host='127.0.0.1', port=8080, debug=False)
+    app.run(host='0.0.0.0', port=8080, debug=False)
 # Вызов функции main
 if __name__ == '__main__':
     main()
