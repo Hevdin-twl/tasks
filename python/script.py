e79a483621bba47c3e14f206a883dea1e7c101f0
VIKTOR VENEDIKTOV <shinijin@gmail.com>
2025-02-11 15:51:45 +0400

    добавлен exception на случай ошибки

--- python/script.py
+++ python/script.py
@@ -60,28 +60,27 @@ def get_service():
     logger.info("Получение сервисов с лейблом: %s", label_selector)
     services = v1.list_service_for_all_namespaces(label_selector=label_selector)
     logger.info("Количество сервисов, найденных: %d", len(services.items))
-
-    service_list = []
-    for service in services.items:
-        service_info = {
-            'name': service.metadata.name,
-            'namespace': service.metadata.namespace,
-            'ports': []
-        }
-        for port in service.spec.ports:
-            service_info['ports'].append({
-                'port': port.port,
-                'protocol': port.protocol
-            })
-        service_list.append(service_info)
-
-    logger.info(service_list)
+    try:
+        service_list = []
+        for service in services.items:
+            service_info = {
+                'name': service.metadata.name,
+                'namespace': service.metadata.namespace,
+                'ports': []
+            }
+            for port in service.spec.ports:
+                service_info['ports'].append({
+                    'port': port.port,
+                    'protocol': port.protocol
+                })
+            service_list.append(service_info)
+        logger.info(service_list)
     
-    # Возвращаем JSON-ответ с кодом 200
-    return jsonify(service_list), 200
-    #except Exception as e:
-    #    logger.error("Ошибка при получении сервисов: %s", str(e))
-    #    return ({'error': str(e)}), 500
+        # Возвращаем JSON-ответ с кодом 200
+        return jsonify(service_list), 200
+    except Exception as e:
+        logger.error("Ошибка при получении сервисов: %s", str(e))
+        return ({'error': str(e)}), 500
     
 # функция считывания переменной окружения времени ожидания перед запуском, задается в окружении среды export STARTUP_DELAY_SECONDS=[seconds]
 def main():
