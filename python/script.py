fe16c9bd59fcb3353e94d3a3b256cdb9b3543432
VIKTOR VENEDIKTOV <shinijin@gmail.com>
2025-02-11 12:26:20 +0400

    багфикс error 500 4

--- python/script.py
+++ python/script.py
@@ -58,25 +58,25 @@ def get_service():
     # определение лейбла сервиса
     label_selector = "app_type=flask-test"
    # try:
-        logger.info("Получение сервисов с лейблом: %s", label_selector)
-        services = v1.list_service_for_all_namespaces(label_selector=label_selector)
-        logger.info("Количество сервисов, найденных: %d", len(services.items))
+    logger.info("Получение сервисов с лейблом: %s", label_selector)
+    services = v1.list_service_for_all_namespaces(label_selector=label_selector)
+    logger.info("Количество сервисов, найденных: %d", len(services.items))
 
-        service_list = []
-        for service in services.items:
-            service_info = {
-                'name': service.metadata.name,
-                'namespace': service.metadata.namespace,
-                'ports': []
-            }
-            for port in service.spec.ports:
-                service_info['ports'].append({
-                    'port': port.port,
-                    'protocol': port.protocol
-                })
-            service_list.append(service_info)
+    service_list = []
+    for service in services.items:
+        service_info = {
+            'name': service.metadata.name,
+            'namespace': service.metadata.namespace,
+            'ports': []
+        }
+        for port in service.spec.ports:
+            service_info['ports'].append({
+                'port': port.port,
+                'protocol': port.protocol
+            })
+        service_list.append(service_info)
 
-        logger.info(service_list), 200
+    logger.info(service_list), 200
     #except Exception as e:
     #    logger.error("Ошибка при получении сервисов: %s", str(e))
     #    return ({'error': str(e)}), 500
