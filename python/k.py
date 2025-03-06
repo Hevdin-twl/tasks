5f6551d70435eb73f25d93f82f501a5987a7e7b8
VIKTOR VENEDIKTOV <shinijin@gmail.com>
2025-03-06 14:26:35 +0400

    В приложение добавлена функция формирования внутрени ДНС имен по лейблу

--- python/k.py
+++ python/k.py
@@ -5,16 +5,16 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def main():
-    config.load_incluster_config()
-    v1 = client.CoreV1Api()
-    
+    # Определение лейбла сервиса
     label_selector = "app_type=flask-test"
+    logger.info("Получение сервисов с лейблом: %s", label_selector)
+    services = v1.list_service_for_all_namespaces(label_selector=label_selector)
+    logger.info("Количество сервисов, найденных: %d", len(services.items))
+    
     try:
-        logger.info("Получение сервисов с лейблом: %s", label_selector)
-        services = v1.list_service_for_all_namespaces(label_selector=label_selector)
-        logger.info("Количество сервисов, найденных: %d", len(services.items))
-
         service_list = []
+        dns_names = []  # Создаем массив для хранения DNS имен
+        
         for service in services.items:
             service_info = {
                 'name': service.metadata.name,
@@ -27,11 +27,21 @@ def main():
                     'protocol': port.protocol
                 })
             service_list.append(service_info)
-
-        return (service_list), 200
+            
+            # Формируем DNS имя и добавляем его в массив dns_names
+            dns_name = f"{service.metadata.name}.default.svc.cluster.local:8080/sleep"
+            dns_names.append(dns_name)  # Добавляем в массив
+        
+        logger.info(service_list)
+        
+        # Выводим значения массива dns_names
+        logger.info("Сформированы следующие DNS имена: %s", dns_names)
+    
+        # Возвращаем JSON-ответ с кодом 200
+        return jsonify(service_list), 200
     except Exception as e:
         logger.error("Ошибка при получении сервисов: %s", str(e))
-        return ({'error': str(e)}), 500
+        return jsonify({'error': str(e)}), 500 
  
 if __name__ == "__main__":
     main()
