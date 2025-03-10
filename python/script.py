a23edc6085095d470b64f32f918d6177ea88d779
VIKTOR VENEDIKTOV <shinijin@gmail.com>
2025-03-10 11:10:18 +0400

    исправление интеграции планировщика в функцию

--- python/script.py
+++ python/script.py
@@ -7,21 +7,21 @@ import random
 import os
 import time
 
-# инициализация Flask
+# Инициализация Flask
 app = Flask(__name__)
 
-# инициализация логгера
+# Инициализация логгера
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# инициализация конфигурации кубера через его api
+# Инициализация конфигурации кубера через его API
 config.load_incluster_config()
 v1 = client.CoreV1Api()
 
-# инициализация планировщика
+# Инициализация планировщика
 scheduler = BackgroundScheduler()
 
-# функция ожидания принимает параметр GET sleep?sleep=[значение]
+# Функция ожидания принимает параметр GET sleep?sleep=[значение]
 @app.route('/sleep', methods=['GET'])
 def sleep_route():
     host_url = request.host_url
@@ -39,60 +39,61 @@ def sleep_route():
     time.sleep(seconds_wait)
     return f"Ждали {seconds_wait} секунд."
 
-# функция проверки доступности вызывается GET параметром /self-test
+# Функция проверки доступности вызывается GET параметром /self-test
 @app.route('/self-test', methods=['GET'])
 def self_test():
     return jsonify({"message": "Self-test successful"}), 200
 
-# функция ожидания перед запуском
+# Функция ожидания перед запуском
 def wait(seconds):
     logger.info(f"Ожидание перед запуском {seconds} секунд...")
     time.sleep(seconds)
     logger.info("Ожидание завершено.")
 
-# создание endpoint для вывода информации от Kubernetes API
+# Создание endpoint для вывода информации от Kubernetes API
 @app.route('/kuber', methods=['GET'])
 def get_service():
-    label_selector = "service_type=flask-test-worker"
-    logger.info("Получение сервисов с лейблом: %s", label_selector)
-    
-    try:
-        services = v1.list_service_for_all_namespaces(label_selector=label_selector)
-        logger.info("Количество сервисов, найденных: %d", len(services.items))
+    with app.app_context():
+        label_selector = "service_type=flask-test-worker"
+        logger.info("Получение сервисов с лейблом: %s", label_selector)
         
-        service_list = []
-        responses = []
-        
-        for service in services.items:
-            service_info = {
-                'name': service.metadata.name,
-                'namespace': service.metadata.namespace,
-                'ports': [{'port': port.port, 'protocol': port.protocol} for port in service.spec.ports]
-            }
-            service_list.append(service_info)
+        try:
+            services = v1.list_service_for_all_namespaces(label_selector=label_selector)
+            logger.info("Количество сервисов, найденных: %d", len(services.items))
+            
+            service_list = []
+            responses = []
+            
+            for service in services.items:
+                service_info = {
+                    'name': service.metadata.name,
+                    'namespace': service.metadata.namespace,
+                    'ports': [{'port': port.port, 'protocol': port.protocol} for port in service.spec.ports]
+                }
+                service_list.append(service_info)
 
-            dns_name = f"http://{service.metadata.name}.default.svc.cluster.local:8080/sleep"
-            try:
-                with urllib.request.urlopen(dns_name) as url_response:
-                    response_data = url_response.read().decode('utf-8')
-                    responses.append(response_data)
-                    logger.info("Cluster Dns name: %s. Response: %s", dns_name, response_data)
-            except Exception as e:
-                logger.error("Ошибка при обращении к %s: %s", dns_name, e)
-                responses.append(f"Error accessing {dns_name}: {e}")
+                dns_name = f"http://{service.metadata.name}.default.svc.cluster.local:8080/sleep"
+                try:
+                    with urllib.request.urlopen(dns_name) as url_response:
+                        response_data = url_response.read().decode('utf-8')
+                        responses.append(response_data)
+                        logger.info("Cluster Dns name: %s. Response: %s", dns_name, response_data)
+                except Exception as e:
+                    logger.error("Ошибка при обращении к %s: %s", dns_name, e)
+                    responses.append(f"Error accessing {dns_name}: {e}")
+            
+            return jsonify({'services': service_list, 'responses': responses})
         
-        return jsonify({'services': service_list, 'responses': responses})
-    
-    except Exception as e:
-        logger.error("Произошла ошибка при получении сервисов: %s", e)
-        return jsonify({'error': 'Ошибка при получении сервисов'}), 500
+        except Exception as e:
+            logger.error("Произошла ошибка при получении сервисов: %s", e)
+            return jsonify({'error': 'Ошибка при получении сервисов'}), 500
 
-# функция планировщика запуска функции get_services с интервалом в 5 секунд
+# Функция планировщика запуска функции get_services с интервалом в 5 секунд
 def start_scheduler():
     scheduler.add_job(func=get_service, trigger='interval', seconds=5)
     scheduler.start()
 
-# функция считывания переменной окружения времени ожидания перед запуском
+# Функция считывания переменной окружения времени ожидания перед запуском
 def main():
     startup_delay = os.environ.get('STARTUP_DELAY_SECONDS')
     logger.info(f"Значение ожидания: {startup_delay}")
