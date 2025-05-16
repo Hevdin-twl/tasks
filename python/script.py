abbcfc3afe0d8234e2176f2b126939a5ce2cbe54
VIKTOR VENEDIKTOV <shinijin@gmail.com>
2025-05-16 12:27:23 +0400

    Рефакторинг кода приложения. Функции разбиты на модули, проверка ошибок осуществляется через отдельную функцию, переменные переименованы.

--- python/script.py
+++ python/script.py
@@ -21,99 +21,109 @@ v1 = client.CoreV1Api()
 # Инициализация планировщика
 scheduler = BackgroundScheduler()
 
-# Функция ожидания принимает параметр GET sleep?sleep=[значение]
-@app.route('/sleep', methods=['GET'])
-def sleep_route():
-    host_url = request.host_url
-    sleep_value = request.args.get('seconds')
-    if not sleep_value:
-        seconds_wait = random.randint(1, 5)
-    else:
-        try:
-            seconds_wait = int(sleep_value)
-            if seconds_wait < 1:
-                raise ValueError("Значение должно быть больше 0.")
-        except ValueError:
-            return "Параметр 'sleep' должен быть целым числом больше 0.", 400
-    logger.info(f"{host_url} Aslept {seconds_wait} seconds")
-    time.sleep(seconds_wait)
-    return f"Ждали {seconds_wait} секунд."
+def setup_environment():
+    """ Настройка и возвращение времени ожидания перед запуском. """
+    startup_delay = os.environ.get('STARTUP_DELAY_SECONDS', 0)
+    try:
+        delay_seconds = max(0, int(startup_delay))
+    except ValueError:
+        logger.warning(f"Некорректное значение переменной окружения: {startup_delay}. Используется значение по умолчанию: 0.")
+        delay_seconds = 0
+    logger.info(f"Значение ожидания перед запуском: {delay_seconds} секунд.")
+    return delay_seconds
 
-# Функция проверки доступности вызывается GET параметром /self-test
-@app.route('/self-test', methods=['GET'])
-def self_test():
-    return jsonify({"message": "Self-test successful"}), 200
-
-# Функция ожидания перед запуском
-def wait(seconds):
+def delay(seconds):
+    """ Ожидание указанного количества секунд с логированием. """
     logger.info(f"Ожидание перед запуском {seconds} секунд...")
     time.sleep(seconds)
     logger.info("Ожидание завершено.")
 
-# Создание endpoint для вывода информации от Kubernetes API
+@app.route('/sleep', methods=['GET'])
+def sleep_route():
+    """ Эндпоинт для ожидания заданного времени. """
+    host_url = request.host_url
+    sleep_value = request.args.get('seconds')
+    seconds_wait = validate_sleep_value(sleep_value)
+
+    if seconds_wait is None:
+        return "Параметр 'sleep' должен быть целым числом больше 0.", 400
+
+    logger.info(f"{host_url} Aslept {seconds_wait} seconds")
+    time.sleep(seconds_wait)
+    
+    return f"Ждали {seconds_wait} секунд."
+
+def validate_sleep_value(sleep_value):
+    """ Проверяет и валидирует значение, переданное в качестве параметра ожидания. """
+    if not sleep_value:
+        return random.randint(1, 5)
+    try:
+        seconds_wait = int(sleep_value)
+        if seconds_wait < 1:
+            raise ValueError("Значение должно быть больше 0.")
+        return seconds_wait
+    except ValueError:
+        return None
+
+@app.route('/self-test', methods=['GET'])
+def self_test():
+    """ Эндпоинт для проверки доступности сервиса. """
+    return jsonify({"message": "Self-test successful"}), 200
+
 @app.route('/kuber', methods=['GET'])
-def get_service():
-    with app.app_context():
-        label_selector = "service_type=flask-test-worker"
-        logger.info("Получение сервисов с лейблом: %s", label_selector)
-        
-        try:
-            services = v1.list_service_for_all_namespaces(label_selector=label_selector)
-            logger.info("Количество сервисов, найденных: %d", len(services.items))
-            
-            service_list = []
-            responses = []
-            
-            for service in services.items:
-                service_info = {
-                    'name': service.metadata.name,
-                    'namespace': service.metadata.namespace,
-                    'ports': [{'port': port.port, 'protocol': port.protocol} for port in service.spec.ports]
-                }
-                service_list.append(service_info)
+def get_services():
+    """ Эндпоинт для получения информации от Kubernetes API о сервисах. """
+    return jsonify(fetch_services_from_k8s())
 
-                dns_name = f"http://{service.metadata.name}.default.svc.cluster.local:8080/sleep"
-                try:
-                    with urllib.request.urlopen(dns_name) as url_response:
-                        response_data = url_response.read().decode('utf-8')
-                        responses.append(response_data)
-                        logger.info("Cluster Dns name: %s. Response: %s", dns_name, response_data)
-                except Exception as e:
-                    logger.error("Ошибка при обращении к %s: %s", dns_name, e)
-                    responses.append(f"Error accessing {dns_name}: {e}")
-            
-            return jsonify({'services': service_list, 'responses': responses})
-        
-        except Exception as e:
-            logger.error("Произошла ошибка при получении сервисов: %s", e)
-            return jsonify({'error': 'Ошибка при получении сервисов'}), 500
+def fetch_services_from_k8s():
+    """ Функция для получения сервисов из Kubernetes. """
+    label_selector = "service_type=flask-test-worker"
+    logger.info("Получение сервисов с лейблом: %s", label_selector)
+    
+    try:
+        services = v1.list_service_for_all_namespaces(label_selector=label_selector)
+        service_list = []
 
-# Функция планировщика запуска функции get_services с интервалом в 5 секунд
+        for service in services.items:
+            service_info = extract_service_info(service)
+            service_list.append(service_info)
+            log_service_response(service_info)
+
+        return {'services': service_list}
+    
+    except Exception as e:
+        logger.error("Произошла ошибка при получении сервисов: %s", e)
+        return {'error': 'Ошибка при получении сервисов'}, 500
+
+def extract_service_info(service):
+    """ Извлекает информацию о сервисе. """
+    return {
+        'name': service.metadata.name,
+        'namespace': service.metadata.namespace,
+        'ports': [{'port': port.port, 'protocol': port.protocol} for port in service.spec.ports]
+    }
+
+def log_service_response(service_info):
+    """ Получает ответ от сервиса и логирует его. """
+    dns_name = f"http://{service_info['name']}.default.svc.cluster.local:8080/sleep"
+    try:
+        with urllib.request.urlopen(dns_name) as url_response:
+            response_data = url_response.read().decode('utf-8')
+            logger.info("Cluster Dns name: %s. Response: %s", dns_name, response_data)
+    except Exception as e:
+        logger.error("Ошибка при обращении к %s: %s", dns_name, e)
+
 def start_scheduler():
-    scheduler.add_job(func=get_service, trigger='interval', seconds=5)
+    """ Запускает планировщик для периодического выполнения функции получения сервисов. """
+    scheduler.add_job(func=fetch_services_from_k8s, trigger='interval', seconds=5)
     scheduler.start()
 
-# Функция считывания переменной окружения времени ожидания перед запуском
 def main():
-    startup_delay = os.environ.get('STARTUP_DELAY_SECONDS')
-    logger.info(f"Значение ожидания: {startup_delay}")
-
-    if startup_delay is not None:
-        try:
-            delay_seconds = int(startup_delay)
-            if delay_seconds < 0:
-                raise ValueError("Значение задержки не может быть отрицательным.")
-        except ValueError as e:
-            logger.info(f"Ошибка: {e}. Используем значение по умолчанию: 0.")
-            delay_seconds = 0
-    else:
-        logger.info("Переменная окружения STARTUP_DELAY_SECONDS не найдена. Используем значение по умолчанию: 0.")
-        delay_seconds = 0
-
-    wait(delay_seconds)
-    start_scheduler()  # Запускаем планировщик
+    """ Главная функция запуска приложения. """
+    delay_seconds = setup_environment()
+    delay(delay_seconds)
+    start_scheduler()
     app.run(host='0.0.0.0', port=8080, debug=False)
 
-# Вызов функции main
 if __name__ == '__main__':
     main()
